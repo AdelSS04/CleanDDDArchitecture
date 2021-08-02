69e46507383865aeaeb082393bae481af1286692
Adel Lajil <adel.lajil@podyam.com>
2021-08-02 10:36:02 +0100

    fix err

--- Infrastructure/RepositoryProduct.cs
+++ Infrastructure/RepositoryProduct.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domaine.Entites;
+using Domaine.RepositoryInterfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,24 @@ namespace Infrastructure
 {
     public class RepositoryProduct : IRepositoryProduct
     {
+        public Task<Article> AddAsync(Article entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Article> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Article> UpdateAsync(Article entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateRangeAsync(List<Article> entities)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
