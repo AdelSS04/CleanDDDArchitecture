5b3e9cbd5a13fb93c7e85838d1bcce7928b287ca
Adel Lajil <adel.lajil@podyam.com>
2021-08-02 10:33:25 +0100

    add generic

---
+++ Domaine/RepositoryInterfaces/IRepositoryProduct.cs
@@ -0,0 +1,14 @@
+﻿using Domaine.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domaine.RepositoryInterfaces
+{
+    public interface  IRepositoryProduct : IRepository<Article>
+    {
+
+    }
+}
---
+++ Infrastructure/Repository.cs
@@ -0,0 +1,32 @@
+﻿using Domaine.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class, new()
+    {
+        public Task<TEntity> AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateRangeAsync(List<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
